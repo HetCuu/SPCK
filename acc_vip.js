9fd1c63486bb9b6b400ef2a60bdfb3752bbec308
HetCuu <minhdepzaivl2020@gmail.com>
2023-09-10 15:52:24 +0700

    spck

---
+++ acc_vip.js
@@ -0,0 +1,62 @@
+const accs = [
+    {
+        cate: "Sale",
+        name: "Acc no 1",
+        price: "1,000,000",
+        image: "https://shopvalorant.com/upload/product/5732582d6debcaccdc2779defe5a31c2.png",
+    },
+    {
+        cate: "Sale",
+        name: "Acc no 2",
+        price: "1,350,000",
+        image: "https://shopvalorant.com/upload/product/38933efffc1d194881a011f1af90fafa.png"
+    },
+    {
+        cate: "Sale",
+        name: "Acc no 3",
+        price: "1,400,000",
+        image: "https://shopvalorant.com/upload/product/008d01f294ad2347197279ef28bbeb58.png"
+    },
+    {
+        cate: "Sale",
+        name: "Acc no 4",
+        price: "2,400,000",
+        image: "https://shopvalorant.com/upload/product/56bd6322e45258c61163670d83db1308.png"
+    },
+    {
+        cate: "Sale",
+        name: "Acc no 5",
+        price: "2,300,000",
+        image: "https://shopvalorant.com/upload/product/49382dcb5828f49abf54cee37a858d79.png"
+    },
+    {
+        cate: "Sale",
+        name: "Acc no 6",
+        price: "1,600,000",
+        image: "https://shopvalorant.com/upload/product/1bd394a1a5fdf63551b15e608100de6e.png"
+    },
+    {
+        cate: "Sale",
+        name: "Acc no 7",
+        price: "1,800,000",
+        image: "https://shopvalorant.com/upload/product/030bda092c9dec69b7b57626305e0a63.png"
+    },
+    {
+        cate: "Sale",
+        name: "Acc no 8",
+        price: "1,500,000",
+        image: "https://shopvalorant.com/upload/product/aaaa71806e76626b8ec2fc68adf950c3.png"
+    },
+    {
+        cate: "Sale",
+        name: "Acc no 9",
+        price: "2,600,000",
+        image: "https://shopvalorant.com/upload/product/70de70652fcd21fea5b4d16f8bd7b1a7.png"
+    },
+    {
+        cate: "Sale",
+        name: "Acc no 10",
+        price: "10,000,000",
+        image: "https://accvalorant.com/backend/web/uploads/images/ACCOUNT/ACC-749.png"
+    },
+];
---
+++ pic.js
@@ -0,0 +1,38 @@
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+
+const id = params.id;
+
+const acc = accs[id];
+document.getElementById("image").src = acc.image;
+document.getElementById("name").innerHTML = acc.name;
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
